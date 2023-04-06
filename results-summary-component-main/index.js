4bc2b91e1848a76959bfdaf46f0d23700352c642
NotPerr <vovo7292@gmail.com>
2023-04-06 23:07:35 +0800

    clean up some code

--- results-summary-component-main/index.js
+++ results-summary-component-main/index.js
@@ -1,22 +1,3 @@
-// function animateValue(obj, start, end, duration) {
-//     let startTimestamp = null;
-//     const step = (timestamp) => {
-//         if (!startTimestamp) startTimestamp = timestamp;
-//         const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-//         obj.innerHTML = Math.floor(progress * (end - start) + start);
-//         if (progress < 1) {
-//             window.requestAnimationFrame(step);
-//         }
-//     };
-//     window.requestAnimationFrame(step);
-// }
-
-// const obj = document.getElementById("score-number");
-
-// setTimeout(() => {
-//     animateValue(obj, 0, 76, 1000)
-// }, 1000);
-
 
 ScrollReveal().reveal(".main");
 ScrollReveal().reveal(".rate",{delay: 1000});
