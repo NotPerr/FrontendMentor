635400c7c8857648975fa2bf18400d6b7c5968e4
NotPerr <vovo7292@gmail.com>
2023-05-26 13:29:21 +0800

    implement advice api

--- advice-generator-app/src/App.js
+++ advice-generator-app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [advice,setAdvice] = useState('');
+
+  function getAdvice() {
+    fetch('	https://api.adviceslip.com/advice')
+   .then(response => response.json())
+   .then(data => {
+     console.log(data);
+     setAdvice(data.slip);
+     console.log(advice);
+   });
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        
+        <br />
+        #{advice.id}
+        <br />
+        {advice === '' ? 'Click the dice to generate advice' : advice.advice}
+        <button onClick={getAdvice}>click me</button>
+      </main>
     </div>
   );
 }
