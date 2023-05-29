1bf4a96c4369ce71155f12838c5c8f3a10dacb0f
NotPerr <vovo7292@gmail.com>
2023-05-29 14:05:10 +0800

    add structure

---
+++ url-shortening-app/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        header
+        <nav className='mobile-nav'>mobile nav</nav>
+      </header>
+
+      <section className='banner'>banner</section>
+
+      <section className='url-input'>url input</section>
+
+      <section className='old-url'>old url</section>
+
+      <section className='info'>info</section>
+
+      <section className='start'>start</section>
+
+      <footer>footer</footer>
+
+
+    </div>
+  );
+}
+
+export default App;
