51169a379eec997aee6fd5068b3362cf140abd38
NotPerr <vovo7292@gmail.com>
2023-05-30 18:53:53 +0800

    fix copy button

--- url-shortening-app/src/ShowUrlList.js
+++ url-shortening-app/src/ShowUrlList.js
@@ -1,23 +1,38 @@
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useState } from 'react';
 
 
-export default function ShowUrlList({urlList}) {
-    if(urlList.length >= 1) {
-        console.log("render old url")
-        return urlList.map(url =>
-          <div key={url.code}>
-            <div>
-              {url.original_link} <br />
-              {url.full_short_link}
-            </div>
-            <CopyToClipboard text={url.full_short_link}>
-              <button>
-                Copy
-              </button>
-            </CopyToClipboard>
+export default function ShowUrlList({ urlList }) {
+    const [copiedIndex, setCopiedIndex] = useState(-1);
+  
+    const handleCopy = (index) => {
+      setCopiedIndex(index);
+    };
+  
+    if (urlList.length >= 1) {
+      const reversedUrlList = [...urlList].reverse();
+
+      return reversedUrlList.map((url, index) => (
+        <div key={url.code}>
+          <div>
+            {url.original_link} <br />
+            {url.full_short_link}
           </div>
-        );
-      }else {
-        return
-      }
-}
+          <CopyToClipboard text={url.full_short_link}>
+            <button onClick={() => handleCopy(index)}>
+              {copiedIndex === index ? 'Copied' : 'Copy'}
+            </button>
+          </CopyToClipboard>
+        </div>
+      ));
+    } else {
+      return null;
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
