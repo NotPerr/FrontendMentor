55dd917ac4227681ebf4167f1a98acdd1c75b688
NotPerr <vovo7292@gmail.com>
2024-04-23 16:32:50 +0800

    created home route

---
+++ rest-countries-api-with-color-theme-switcher-master/countries_app/src/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ rest-countries-api-with-color-theme-switcher-master/countries_app/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/Home.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
